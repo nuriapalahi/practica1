a4d43ac173bac316fdd8e9d26bd7a0cc2af9fd0a
nuriapalahi <nuriapalahi@gmail.com>
2017-11-12 00:53:08 +0100

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,17 @@
+import json
+import urllib
+
+symbolslist = open("symbols_less.txt").read()
+symbolslist = symbolslist.split("\n")
+for symbol in symbolslist:
+    myfile = open("daily_prices/" + symbol + ".txt", "w+")
+    myfile.close()
+
+    htmltext = urllib.urlopen("http://www.bloomberg.com/markets/chart/data/1D/" + symbol + ":SM")
+    data = json.load(htmltext)
+    datapoints = data['data_values']
+
+    myfile = open("daily_prices/" + symbol + ".txt","a")
+    for point in datapoints:
+        myfile.write(str(symbol + "," + str(point[0]) + "," + str(point[1]) + "\n"))
+    myfile.close()
